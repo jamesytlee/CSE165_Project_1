47f6d4c3f2b630d4b164a438f206b985170ca8f3
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-25 17:58:37 -0700

    Adds chair and desk spawning

--- Assets/Scripts/RayCasting.cs
+++ Assets/Scripts/RayCasting.cs
@@ -6,14 +6,26 @@ public class RayCasting : MonoBehaviour
 {
     [SerializeField] protected OVRInput.Controller controller;
 
+    // Variables for Ray Casting
     [SerializeField] protected float rayLength = 10f;
     [SerializeField] protected LineRenderer lineRenderer;
     private RaycastHit hit;
+
+    // Tag for detecting the floor
+    [SerializeField] private string floorTag = "Floor";
+
+    // Variables for Object Spawning
+    [SerializeField] protected GameObject chair;
+    [SerializeField] protected GameObject desk;
+    public Quaternion rotation = Quaternion.Euler(-90f, -180f, 90f);
+    public Vector3 yOffset = new Vector3(0, 0.152f, 0);
 
     // Update is called once per frame
     void Update()
     {
         CastRayFromController();
+        SpawnChair();
+        SpawnDesk();
     }
 
     private void CastRayFromController()
@@ -31,6 +43,33 @@ public class RayCasting : MonoBehaviour
         else
         {
             lineRenderer.SetPosition(1, rayOrigin + rayDirection * rayLength);
+        }
+    }
+
+    private void SpawnChair()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One, controller))
+        {
+            // Check if the ray hits the floor
+            if (hit.collider != null && hit.collider.CompareTag(floorTag))
+            {
+                // Spawn chair prefab at intersection point
+                Instantiate(chair, hit.point, rotation);
+            }
+        }
+    }
+
+    private void SpawnDesk()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two, controller))
+        {
+            // Check if the ray hits the floor
+            if (hit.collider != null && hit.collider.CompareTag(floorTag))
+            {
+                // Spawn chair prefab at intersection point
+                // y-offset = .152
+                Instantiate(desk, hit.point + yOffset, rotation);
+            }
         }
     }
 }
